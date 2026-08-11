bbdb2d588a2efbea9af19e35c900bc73a1af0ffb
Babar Binyamin <babarbinyamin2001@gmail.com>
2026-08-11 17:36:15 +0500

    updated

--- frontend/src/pages/Students.jsx
+++ frontend/src/pages/Students.jsx
@@ -5,6 +5,8 @@ const Students = () => {
     <div>
       <h1>Students</h1>
       <p>Ii I am the student of Islamia University of Bahawalpur</p>
+            <p>Ii I am the student of Islamia University of Bahawalpur</p>
+
     </div>
   )
 }
